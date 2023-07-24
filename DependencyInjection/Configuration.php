d48eac35a88d67cbaf97bafe679b738231b58f97
HOMEdigital <41010376+HOMEdigital@users.noreply.github.com>
2023-07-24 12:07:21 +0200

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('home_completee');
+        $treeBuilder = new TreeBuilder('home_completee');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
